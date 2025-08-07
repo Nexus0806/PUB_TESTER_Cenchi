166e194a0318655e2f7703ac9ec10dc60b797a67
leemg <sjdkd32@naver.com>
2025-08-07 10:32:48 +0900

    VO 내용 수정
    VO에 Heidi에 있는 내용들 넣음

--- src/main/java/egovframework/pubtest/service/UserVO.java
+++ src/main/java/egovframework/pubtest/service/UserVO.java
@@ -1,37 +1,137 @@
-
 package egovframework.pubtest.service;
 
+import java.util.List;
+import java.util.Date;
+
 public class UserVO {
-    // 회원 정보
-    private String name;
-    private String email;
-    private String password;
-
-    // 체험단 정보
-    private String expId;
-    private String title;
-    private String content;
-    private String regDate;
-
-    // Getters and Setters
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
-    public String getExpId() { return expId; }
-    public void setExpId(String expId) { this.expId = expId; }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-
-    public String getRegDate() { return regDate; }
-    public void setRegDate(String regDate) { this.regDate = regDate; }
+    
+	private int userIdx;
+    private Date userRegdate;
+    private String userName;
+    private String userNickname;
+    private String userProfile;
+    private String userPhoNum;
+    private String userIntro;
+    private String userEmail;
+    private Date userBirth;
+    private String userSex;
+    private String userArea;
+    private String userAct;
+    private String userBlog;
+    private String userInsta;
+    private String userYoutu;
+    private String userTiktok;
+    private String userEtc;
+    private String userAddress;
+    
+    
+	public int getUserIdx() {
+		return userIdx;
+	}
+	public void setUserIdx(int userIdx) {
+		this.userIdx = userIdx;
+	}
+	public Date getUserRegdate() {
+		return userRegdate;
+	}
+	public void setUserRegdate(Date userRegdate) {
+		this.userRegdate = userRegdate;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserNickname() {
+		return userNickname;
+	}
+	public void setUserNickname(String userNickname) {
+		this.userNickname = userNickname;
+	}
+	public String getUserProfile() {
+		return userProfile;
+	}
+	public void setUserProfile(String userProfile) {
+		this.userProfile = userProfile;
+	}
+	public String getUserPhoNum() {
+		return userPhoNum;
+	}
+	public void setUserPhoNum(String userPhoNum) {
+		this.userPhoNum = userPhoNum;
+	}
+	public String getUserIntro() {
+		return userIntro;
+	}
+	public void setUserIntro(String userIntro) {
+		this.userIntro = userIntro;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public Date getUserBirth() {
+		return userBirth;
+	}
+	public void setUserBirth(Date userBirth) {
+		this.userBirth = userBirth;
+	}
+	public String getUserSex() {
+		return userSex;
+	}
+	public void setUserSex(String userSex) {
+		this.userSex = userSex;
+	}
+	public String getUserArea() {
+		return userArea;
+	}
+	public void setUserArea(String userArea) {
+		this.userArea = userArea;
+	}
+	public String getUserAct() {
+		return userAct;
+	}
+	public void setUserAct(String userAct) {
+		this.userAct = userAct;
+	}
+	public String getUserBlog() {
+		return userBlog;
+	}
+	public void setUserBlog(String userBlog) {
+		this.userBlog = userBlog;
+	}
+	public String getUserInsta() {
+		return userInsta;
+	}
+	public void setUserInsta(String userInsta) {
+		this.userInsta = userInsta;
+	}
+	public String getUserYoutu() {
+		return userYoutu;
+	}
+	public void setUserYoutu(String userYoutu) {
+		this.userYoutu = userYoutu;
+	}
+	public String getUserTiktok() {
+		return userTiktok;
+	}
+	public void setUserTiktok(String userTiktok) {
+		this.userTiktok = userTiktok;
+	}
+	public String getUserEtc() {
+		return userEtc;
+	}
+	public void setUserEtc(String userEtc) {
+		this.userEtc = userEtc;
+	}
+	public String getUserAddress() {
+		return userAddress;
+	}
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
 }
+	
