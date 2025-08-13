385d520cdc276a96ff3c23c6b2d8837be2a3e312
Cenchi185 <kangmin7840@naver.com>
2025-08-13 18:04:28 +0900

    퇴근전 마지막 커밋

--- src/main/java/egovframework/pubtest/mypage/serivce/impl/MypageDAO.java
+++ src/main/java/egovframework/pubtest/mypage/serivce/impl/MypageDAO.java
@@ -1,9 +1,16 @@
 package egovframework.pubtest.mypage.serivce.impl;
 
+import java.util.List;
+
 import org.egovframe.rte.psl.dataaccess.EgovAbstractMapper;
 import org.springframework.stereotype.Repository;
+
+import egovframework.pubtest.campaign.service.CampaignVO;
 
 @Repository("mypageDAO")
 public class MypageDAO extends EgovAbstractMapper{
 
+	public List<CampaignVO> getUserCampList (int idx) {
+		return selectList("mypageDAO.getUserCampList", idx);
+	}
 }
